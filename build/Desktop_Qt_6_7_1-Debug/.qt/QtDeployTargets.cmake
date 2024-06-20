e8ac96caef7d412366504b668b5a2aafb33b34af
2022051615265diaoliangwei <1772025978@qq.com>
2024-06-20 22:33:56 +0800

    search function

--- build/Desktop_Qt_6_7_1-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,1 +0,0 @@
-set(__QT_DEPLOY_TARGET_appfreems_FILE /root/qml/freems/build/Desktop_Qt_6_7_1-Debug/appfreems)
--- build/Desktop_Qt_6_7_1-Debug/CMakeFiles/appfreems.dir/appfreems_autogen/EWIEGA46WW/qrc_images.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/appfreems.dir/appfreems_autogen/EWIEGA46WW/qrc_images.cpp.o: \
-  /root/qml/freems/build/Desktop_Qt_6_7_1-Debug/appfreems_autogen/EWIEGA46WW/qrc_images.cpp
